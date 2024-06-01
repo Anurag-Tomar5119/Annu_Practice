871c8f80045359cf920a362e85efb0ed56156d42
Anurag-Tomar5119 <anuragtomar5119@gmail.com>
2024-06-01 22:41:04 +0530

    nothing

---
+++ Annu.js
@@ -0,0 +1,25 @@
+function createPattern(rows) {
+    let pattern = [];
+
+    for (let i = 1; i <= rows; i++) {
+        let row = [];
+        let currentNumber = i;
+        for (let j = 1; j <= i; j++) {
+            row.push(currentNumber);
+            currentNumber += rows - j;
+        }
+        pattern.push(row);
+    }
+
+    return pattern;
+}
+
+function displayPattern(pattern) {
+    for (let i = 0; i < pattern.length; i++) {
+        console.log(pattern[i].join("\t"));
+    }
+}
+
+const numRows = 5;
+const generatedPattern = createPattern(numRows);
+displayPattern(generatedPattern);
